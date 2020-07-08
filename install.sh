bc6636a318e1648d03be98d6ace4e2928f6cbd82
Luis Adrián Chávez Fregoso <lacf95@localhost.localdomain>
2020-07-08 18:30:46 -0500

    Creates basic installation script

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env zsh
+
+# Basic configuration
+mkdir -p ~/.config
+
+# Zsh
+if [ -f ~/.zshrc ]; then
+  cat <(echo) ./.zshrc >> ~/.zshrc
+else
+  cp ./.zshrc_shebang >> ~/.zshrc
+fi
+
+cp -r ./zsh ~/.config/zsh
+
+# Prezto
+setopt EXTENDED_GLOB
+for rcfile in ~/.config/zsh/zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "~/.config/zsh/.${rcfile:t}"
+done
+
+# Tmux configuration
+if [ -f ~/.tmux.conf ]; then
+  cat <(echo) ./.tmux.conf >> ~/.tmux.conf
+else
+  cp ./.tmux.conf >> ~/.tmux.conf
+fi
+
+cp -r ./tmux ~/.config/tmux
+
+# Neovim
+cp -r ./nvim ~/.config/nvim
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+nvim -E -s -u ~/.config/nvim/init.vim +PlugInstall +qall
+
+# NERDFont SauceCodePro mono
+mkdir -p ~/.local/share/fonts
+cp ./fonts/* ~/.local/share/fonts
---
+++ zshrc
@@ -0,0 +1,6 @@
+export ZDOTDIR=~/.config/zsh
+export TMUXDOTDIR=~/.config/tmux
+
+if [ -f $ZDOTDIR/.zshrc ]; then
+  source $ZDOTDIR/.zshrc
+fi
